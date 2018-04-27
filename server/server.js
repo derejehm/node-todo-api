808bbf20fd73cf1fad1f51be5ec4d553519847b9
Derejehm <hailemariam013@gmail.com>
2018-04-27 06:50:14 -0700

    password hash added

--- server/server.js
+++ server/server.js
@@ -1,14 +1,17 @@
 //Import modules
-var {ObjectID}=require('mongodb');
-
-var express=require('express');
-var bodyParser=require('body-parser')
+const _=require('lodash');
+const {ObjectID}=require('mongodb');
+const express=require('express');
+const bodyParser=require('body-parser')
 
 var {mongoose} = require('./db/mongoose');
 var {Todo}=require('./models/todo');
 var {User}=require('./models/user');
+ var {authenticate}=require('./middleware/authenticate')
 
 var app=express();
+
+var port=process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -48,9 +51,74 @@ app.get('/todos/:id',(req,res)=>{
  }).catch((e)=>{
   res.status(400).send();
 });
+});
+
+app.delete('/todos/:id',(req,res)=>{
+  var id=req.params.id;
+  if(!ObjectID.isValid(id))
+  {
+  return  res.status(404).send();
+  }
+ Todo.findByIdAndRemove(id).then((todo)=>{
+   if(!todo)
+   {
+     return res.status(404).send();
+   }
+   res.send({todo});
+ }).catch((e)=>{
+   res.status(400).send();
+ });
+});
+
+app.patch('/todos/:id', (req,res)=>{
+  var id=req.params.id;
+  var body=_.pick(req.body,['text','completed']);
+  if(!ObjectID.isValid(id))
+  {
+  return  res.status(404).send();
+  }
+
+  if(_.isBoolean(body.completed)&& body.completed)
+  {
+   body.compltedAt=new Date().getTime();
+  }else {
+    body.completed=false;
+    body.compltedAt=null;
+  }
+
+  Todo.findByIdAndUpdate(id,{$set:body},{new:true}).then((todo)=>{
+    if(!todo)
+    {
+      return res.status(404).send();
+    }
+    res.send({todo});
+
+  }).catch((e)=>{
+    res.status(400).send();
+  });
 
 });
 
-app.listen(3000,()=>{
-  console.log('Starting on port 3000');
+//User
+app.post('/users',(req,res)=>{
+ var body=_.pick(req.body,['email','password']);
+ var user=new User(body);
+
+ user.save().then(()=>{
+  return user.generateAuthToken();
+ }).then((token)=>{
+   res.header('x-auth',token).send(user);
+ }).catch((e)=>{
+   res.status(400).send(e);
+ });
+});
+
+
+
+app.get('/users/me',authenticate,(req,res)=>{
+  res.send(req.user);
+});
+
+app.listen(port,()=>{
+  console.log(`Starting on port ${port}`);
 });
